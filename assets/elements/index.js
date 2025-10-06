e749e1d979531febdb4342ca31278c8c142b6c05
Marc-Antoine Lacasse <m.a.l.musique@gmail.com>
2025-10-06 15:59:05 -0400

    feat: add spotlight component

--- assets/elements/index.js
+++ assets/elements/index.js
@@ -1,5 +1,7 @@
 import CarouselComponent from "./CarouselComponent.js";
 import PuzzleCaptcha from "./PuzzleCaptcha.js";
+import Spotlight from "./Spotlight.js";
 
 customElements.define("carousel-component", CarouselComponent);
 customElements.define("puzzle-captcha", PuzzleCaptcha);
+customElements.define("spotlight-bar", Spotlight);
