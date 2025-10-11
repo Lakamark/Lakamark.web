bcfe1f793b076fcb1c73bf0f7241fb447b648c1c
Marc-Antoine Lacasse <m.a.l.musique@gmail.com>
2025-10-11 18:32:24 -0400

    style: create the dashboard layout
    BREAKING CHANGE: create a vars.scss file to use for the frontend and the dashboard. Both parts of the website use some common CSS variables.

--- assets/dashboard.js
+++ assets/dashboard.js
@@ -4,41 +4,27 @@
 
 import './css/dashboard.scss';
 import './elements/index.js'
-import {disableBodyScroll, enableBodyScroll} from "body-scroll-lock";
 
+const bodyElement = document.body;
+const btnSlidebarToggler = document.getElementById('js-main-header-toggler');
+const btnSwitcherToggler = document.getElementById('js-theme-switcher');
+const btnCollapseToggler = document.getElementById('js-sidebar-collapse');
 
-// Sidebar trigger
-const sidebarTrigger = document.getElementById('js-toggle-menu');
-const sidebar = document.getElementById('js-sidebar-collapse');
-
-sidebarTrigger.addEventListener('click', () => {
-    let isOpen = false;
-    let bodyContent = document.querySelector('body');
-    bodyContent.classList.toggle('sidebar-open');
-    isOpen ? enableBodyScroll(bodyContent) : disableBodyScroll(bodyContent);
-});
-
-// Sidebar collapse
-const sidebarCollapse = () => {
-    document.body.classList.toggle('sidebar-collapsed');
+// Hide or show the sidebar (only on mobile devices)
+// Sidebar toggle
+const sidebarToggle = () => {
+    bodyElement.classList.toggle('sidebar-open');
 }
 
-sidebar.addEventListener('click', sidebarCollapse)
+// Switch between day theme and the night theme
+btnSlidebarToggler.addEventListener('click', sidebarToggle)
+btnSwitcherToggler.addEventListener('click', () => {
+    bodyElement.classList.toggle('day-theme');
+})
 
-
-
-// Theme switcher
-const switcherTheme = () => {
-    // Get the root element and data-theme value
-    const rootElement = document.documentElement;
-    let dataTheme = rootElement.getAttribute('data-theme'), newTheme
-
-    newTheme = (dataTheme === 'light' ? 'dark' : 'light');
-    rootElement.setAttribute('data-theme', newTheme);
-
-    // Set the new local storage item
-    localStorage.setItem('theme', newTheme);
+// Collapse the sidebar
+const sidebarCollapseToggle = () => {
+    bodyElement.classList.toggle('sidebar-collapse');
 }
 
-// Add eventListener
-document.querySelector('#js-sidebar-switcher').addEventListener('click', switcherTheme);
+btnCollapseToggler.addEventListener('click', sidebarCollapseToggle)
